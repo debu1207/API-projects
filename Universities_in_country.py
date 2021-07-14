38d2f94d0876be5cc24bd63b0e21f01949209aea
Debu Patra <patradebu1207@gmail.com>
2021-07-14 20:34:59 +0530

    adding icon and modifiying code

--- Universities_in_country.py
+++ Universities_in_country.py
@@ -1,7 +1,7 @@
-from tkinter.constants import BOTH, BOTTOM, CENTER, END, LEFT, RIGHT, Y
+from tkinter.constants import BOTH, END, RIGHT, Y
 import requests
-import time
 import tkinter as tk
+from tkinter import ttk
 import webbrowser
 
 chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
@@ -18,7 +18,6 @@ def clear():
 def Go_to_University_website(canvas):
     college = mylist.curselection()
     college_id = college[0]
-    print(college_id)
     url = response[college_id]["web_pages"][0]
     webbrowser.get(chrome_path).open(url)
 
@@ -29,7 +28,7 @@ def getWeather(canvas):
     global response
     api = "	http://universities.hipolabs.com/search?country=" + country
     response = requests.get(api).json()
-    print(type(response))
+
     total_university = len(response)
     university.config(text="Total number of Universities / Colleges: " + str(total_university))
     for i in range(len(response)):
@@ -37,17 +36,21 @@ def getWeather(canvas):
         mylist.insert(END, data)
 
 
+
 canvas = tk.Tk()
 canvas.geometry("600x500")
 canvas.title("University List app")
+icon = tk.PhotoImage(file='static/mortarboard.png')
 
-detail_font = ("poppins", 10, "bold")
-font = ("poppins", 20, "bold")
+# Setting icon of master window
+canvas.iconphoto(False, icon)
+detail_font = ("Times", 15, "bold")
+font = ("Times", 20, "bold")
 
 label = tk.Label(canvas, font=detail_font)
 label.config(text="Enter country name: ")
-label.pack(pady=20)
-textfield = tk.Entry(canvas, justify='center', fg='blue', font=font)
+label.pack(pady=15)
+textfield = tk.Entry(canvas, justify='center', fg='green', font=font)
 textfield.pack()
 textfield.focus()
 textfield.bind('<Return>', getWeather)
@@ -55,8 +58,8 @@ textfield.bind('<Return>', getWeather)
 university = tk.Label(canvas, font=detail_font)
 university.pack()
 
-button = tk.Button(canvas, text="clear", command=clear)
-button.place(x=500, y=65)
+button = tk.Button(canvas, text="Clear", command=clear)
+button.place(x=500, y=59)
 
 sb = tk.Scrollbar(canvas)
 sb.pack(side=RIGHT, fill=BOTH)
